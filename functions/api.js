6f51468fae5b7ea028b3b7a86aaaa0bea919b625
[Ali Raza Shaheb] <[arz.shaheb@gmail.com]>
2022-10-30 16:48:17 +0530

    fix frontend

--- functions/api.js
+++ functions/api.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 router.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.json({ msg: "hello world" });
 });
 router.get("/ali", (req, res) => {
   res.json({ msg: name });
